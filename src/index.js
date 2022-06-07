2803229a778e2b76d8d19b178659b5a97f1690b1
OAINTERNAL\fredicson.garcia <fredicson99@gmail.com>
2022-06-07 15:37:08 -0500

    port

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const  morgan = require('morgan');
 const  cors = require('cors');
 const app =  express();
 
-app.set('port',process.env.PORT || 4000)
-
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
+let PORT = process.env.PORT || 4000
+
 app.use(require('./Routes/testApi/testService'))
-app.listen(app.get('port'), ()=>{
-    console.log('is executed')
+app.listen(PORT, ()=>{
+    console.log('is executed '+ PORT)
 })
